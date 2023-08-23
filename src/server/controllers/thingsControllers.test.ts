2e6e56e5f100a6e0b8984561170aa2e8f7d61fd4
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-23 19:43:34 +0200

    Add morgan-debug-mongoose files and test getThings.ts

--- src/server/controllers/thingsControllers.test.ts
+++ src/server/controllers/thingsControllers.test.ts
@@ -1,18 +1,25 @@
 import { type Request, type Response } from "express";
 import { getThings } from "./thingsControllers.js";
+
+const res: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+
 describe("Given a getThings controller", () => {
   describe("When it receives a response", () => {
+    const req: Partial<Request> = {};
+    const expectedStatusCode = 200;
     test("Then it should call its method status with 200", () => {
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-      const expectedStatusCode = 200;
-
       getThings(req as Request, res as Response);
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call its method json", () => {
+      getThings(req as Request, res as Response);
+
+      expect(res.json).toHaveBeenCalled();
     });
   });
 });
