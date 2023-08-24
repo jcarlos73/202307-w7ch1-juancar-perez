74107a7b53cc7d5f660d482710944ec12f6f4bc5
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-24 10:45:47 +0200

    Create errors.ts to middlewares folder and import to routers index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import {
+  endpointNotFound,
+  generalErrorHandler,
+} from "./controllers/middlewares/errors.js";
+import { pingController } from "./controllers/ping/pingController.js";
 import thingsRouters from "./routers/thingsRouters.js";
 
 const app = express();
@@ -7,6 +12,11 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("/", pingController);
+
 app.use("/things", thingsRouters);
+
+app.use(endpointNotFound);
+app.use(generalErrorHandler);
 
 export default app;
